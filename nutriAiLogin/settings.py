75db6a0abc90da5eda9eb8366b1b0328ec96d2f4
motta-phenrique <motta.phenrique@gmail.com>
2024-05-16 17:28:08 -0300

    atualização de lingua

--- nutriAiLogin/settings.py
+++ nutriAiLogin/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
 
     # Optional -- requires install using `django-allauth[socialaccount]`.
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google'
 ]
 
 SITE_ID = 1
