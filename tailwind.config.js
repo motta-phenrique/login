8d887492641a795a1ed387a4e2a8e313c7abf4bb
motta-phenrique <motta.phenrique@gmail.com>
2024-05-16 16:48:25 -0300

    primeira att

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // ou 'media' ou 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
